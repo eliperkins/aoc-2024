7da44d6257e9e106ec40a01d076f6ffd60bc2e07
Eli Perkins <eli.j.perkins@gmail.com>
2025-12-01 17:25:28 -0700

    Update Actions

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -7,3 +7,9 @@ for ((i=1; i<=day; i++)); do
     echo "Fetching day $i input..."
     swift run --skip-build aoc-cli fetch -d $i -y 2024
 done
+
+day=$(ls -1v Sources/AdventOfCode2025 | grep -E 'Day[0-9]+.swift' | sort -V | tail -1 | grep -oE '[0-9]+')
+for ((i=1; i<=day; i++)); do
+    echo "Fetching day $i input..."
+    swift run --skip-build aoc-cli fetch -d $i -y 2025
+done
