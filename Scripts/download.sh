c84bd0fb4213deacfc3e81f84f09b00e11a0269e
Eli Perkins <eli.j.perkins@gmail.com>
2025-12-11 14:26:35 -0800

    Fetch inputs in parallel

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-swift build --product aoc-cli
+swift build --product aoc-cli -c release
 
 day=$(ls -1v Sources/AdventOfCode2024 | grep -E 'Day[0-9]+.swift' | sort -V | tail -1 | grep -oE '[0-9]+')
 for ((i=1; i<=day; i++)); do
     echo "Fetching day $i input..."
-    swift run --skip-build aoc-cli fetch -d $i -y 2024
+    ./.build/release/aoc-cli fetch -d $i -y 2024 &
 done
 
 day=$(ls -1v Sources/AdventOfCode2025 | grep -E 'Day[0-9]+.swift' | sort -V | tail -1 | grep -oE '[0-9]+')
 for ((i=1; i<=day; i++)); do
     echo "Fetching day $i input..."
-    swift run --skip-build aoc-cli fetch -d $i -y 2025
+    ./.build/release/aoc-cli fetch -d $i -y 2025 &
 done
+
+wait
