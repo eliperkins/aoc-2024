24dbb8e494e72f3e560d585bd2f03ec86645e391
Eli Perkins <eli.j.perkins@gmail.com>
2025-12-12 10:03:50 -0700

    Revert "Fetch inputs in parallel"
    This reverts commit c84bd0fb4213deacfc3e81f84f09b00e11a0269e.

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-swift build --product aoc-cli -c release
+swift build --product aoc-cli
 
 day=$(ls -1v Sources/AdventOfCode2024 | grep -E 'Day[0-9]+.swift' | sort -V | tail -1 | grep -oE '[0-9]+')
 for ((i=1; i<=day; i++)); do
     echo "Fetching day $i input..."
-    ./.build/release/aoc-cli fetch -d $i -y 2024 &
+    swift run --skip-build aoc-cli fetch -d $i -y 2024
 done
 
 day=$(ls -1v Sources/AdventOfCode2025 | grep -E 'Day[0-9]+.swift' | sort -V | tail -1 | grep -oE '[0-9]+')
 for ((i=1; i<=day; i++)); do
     echo "Fetching day $i input..."
-    ./.build/release/aoc-cli fetch -d $i -y 2025 &
+    swift run --skip-build aoc-cli fetch -d $i -y 2025
 done
-
-wait
